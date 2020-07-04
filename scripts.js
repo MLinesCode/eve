93bc27a7b6f54bf723f1a1a3f3c02adf8e6fb00d
Specter <mikexx1264@gmail.com>
2020-07-04 03:49:13 -0500

    Nueva estructura del proyecto js/css comprimidos

--- scripts.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector(".hamburger-menu");
-const navbar = document.querySelector(".nav-bar");
-
-menuIcon.addEventListener("click", () => {
-  navbar.classList.toggle("change");
-});
